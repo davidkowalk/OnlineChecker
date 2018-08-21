782a38ca248278227d6aecea57fef65af3b54bae
David J. Kowalk <david.kowalk@gmail.com>
2018-08-21 13:23:24 +0200

    Added Errors
    Exits with:
    - 0 if you are online
    - 1 if you are offline
    - 2 if Google seems to be offline
    -3 if you are offline and your network card fails to connect to itself.

--- onlinecheck.sh
+++ onlinecheck.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 err=0
-errg=0
-errn=0
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -17,25 +15,32 @@ then
 	echo
 	echo "You are Online"
 else
-
+	
+	err=2
+	
 	echo
 	echo "Couldn't connect to Google"
 	echo "Pinging Reddit..."
-
+	
 	if( ping reddit.com > /dev/null)
 	then
 		echo
 		echo -e "${GREEN}You are Online${NC}"
 	else
+		err=1
+		
 		echo
 		echo -e "${RED}You are Offline${NC}"
 
 		if (! ping -c 1 127.0.0.1 > /dev/null)
 		then
+			err=3
 			echo
-			echo "Network card not properly installed"
+			echo "Network card might not be properly installed"
 		fi
 	fi
 fi
 
 echo
+
+exit $err
